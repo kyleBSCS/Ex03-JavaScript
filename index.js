e59f32b6fa5af09f15dbf2b8be081f18c4589273
kyleBSCS <kpvinuya@up.edu.ph>
2024-02-20 06:22:09 +0800

    feat: implemented function for validating password

--- index.js
+++ index.js
@@ -4,7 +4,21 @@
 // DESCRIPTION: Exercie 3 | Javascript Basics (Password Validation)
 
 const validate = (pass1, pass2) => {
+    if (pass1 !== pass2) return false;
+    if (pass1.length < 8) return false;
+    
+    let conditions = [false, false, false]
+    for (let i = 0; i < pass1.length; i++) {
+        if (pass1.charAt(i) === pass1.charAt(i).toUpperCase() && isNaN(pass1.charAt(i))) conditions[0] = true
+        if (pass1.charAt(i) === pass1.charAt(i).toLowerCase() && isNaN(pass1.charAt(i))) conditions[1] = true;
+        if (!isNaN(Number(pass1.charAt(i)))) conditions[2] = true;
+    }
 
+    let finalCondition = true;
+    conditions.forEach((element) => {if (element === false) finalCondition = false});
+
+    if (finalCondition) return true;
+    else return false;
 }
 
 const reverse = (pass) => {
@@ -14,3 +28,9 @@ const reverse = (pass) => {
 const convert = (name, pass1, pass2) => {
 
 }
+
+console.log(validate("helloworld","hello"))
+console.log(validate("hello","hello"))
+console.log(validate("hello1234","hello1234"))
+console.log(validate("Hello1234","Hello1234"))
+console.log(validate("HELLO1234","HELLO1234"))
