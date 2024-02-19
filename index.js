37f18a02c2ff7eeb195be24b00cc03c49fdefc75
kyleBSCS <kpvinuya@up.edu.ph>
2024-02-20 05:55:15 +0800

    chore: fixed last commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// NAME: Kyle Nathaniel P. Vinuya
+// STUDENT NUMBER: 2022-69573
+// SECTION: CMSC 100 - U3L
+// DESCRIPTION: Exercie 3 | Javascript Basics (Password Validation)
+
+const validate = (pass1, pass2) => {
+
+}
+
+const reverse = (pass) => {
+
+}
+
+const convert = (name, pass1, pass2) => {
+
+}
